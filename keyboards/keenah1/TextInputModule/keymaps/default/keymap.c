7f1524eb596f279ec49708cfdd7ec5bad098a698
keenah1 <jordan.a.rodier@gmail.com>
2024-05-23 07:18:40 -0500

    Swap lshift and bspc, enter and bspc

--- keyboards/keenah1/TextInputModule/keymaps/default/keymap.c
+++ keyboards/keenah1/TextInputModule/keymaps/default/keymap.c
@@ -38,22 +38,22 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      *              ┌─────┬─────┬─────┬─────┬─────────┐         ┌─────┬─────┬──────┬─────┬─────┐
      *              │ 1/! │ 2/@ │ 3/# │ 4/$ │   5/%   │         │ 6/^ │ 7/& │ 8\*  │ 9/( │ 0/) │
-     *       ┌──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼──────┐
-     *       │ TAB  │  Q  │  W  │  E  │  R  │    T    │         │  Y  │  U  │  I   │  O  │  P  │ BSPC │
-     * ┌─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼──────┼─────┐
-     * │ ESC │ BSPC │  A  │  S  │  D  │  F  │    G    │         │  H  │  J  │  K   │  L  │ ;/: │ '/"  │ ENT │
-     * ├─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼──────┼─────┤
-     * │ WIN │ SFT  │  Z  │  X  │  C  │  V  │    B    │         │  N  │  M  │ ,/<  │ ./> │ //? │ \/|  │ DEL │
-     * └─────┴──────┴─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┴──────┴─────┘
-     *                    │ VSC │ L1  │ SPC │ ENT/CTL │         │     │ SPC │ SPCL │     │
+     *       ┌──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼─────┐
+     *       │ TAB  │  Q  │  W  │  E  │  R  │    T    │         │  Y  │  U  │  I   │  O  │  P  │ ENT │
+     * ┌─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼─────┼──────┐
+     * │ ESC │ SFT  │  A  │  S  │  D  │  F  │    G    │         │  H  │  J  │  K   │  L  │ ;/: │ '/" │ BSPC │
+     * ├─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┼─────┼──────┤
+     * │ WIN │ BSPC │  Z  │  X  │  C  │  V  │    B    │         │  N  │  M  │ ,/<  │ ./> │ //? │ \/| │ DEL  │
+     * └─────┴──────┴─────┼─────┼─────┼─────┼─────────┤         ├─────┼─────┼──────┼─────┼─────┴─────┴──────┘
+     *                    │ VSC │ L1  │ SPC │ ENT/CTL │         │ SPC │ ENT │ SPCL │     │
      *                    └─────┴─────┴─────┴─────────┘         └─────┴─────┴──────┴─────┘
      */
     LAYOUT(
-                        KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,  KC_7,   KC_8,           KC_9,   KC_0,
-               KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,  KC_U,   KC_I,           KC_O,   KC_P,     KC_BSPC,
-      KC_ESC,  KC_BSPC, KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,  KC_J,   KC_K,           KC_L,   KC_SCLN,  KC_QUOT,  KC_ENT,
-      KC_LWIN, KC_LSFT, KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,  KC_M,   KC_COMM,        KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
-                                 MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_NO, KC_SPC, MO(LAYER_SPCL), KC_NO
+                        KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,   KC_8,           KC_9,   KC_0,
+               KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,           KC_O,   KC_P,     KC_ENT,
+      KC_ESC,  KC_LSFT, KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,   KC_K,           KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
+      KC_LWIN, KC_BSPC, KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,        KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
+                                 MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_SPC, KC_ENT, MO(LAYER_SPCL), KC_NO
     ),
 
   [LAYER_1] =
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
      *       ┌─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┐
      *       │ ALT+TAB │     │     │ UP  │ PDN │ END │         │ F4  │ F5  │ F6  │     │     │     │
      * ┌─────┼─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┐
-     * │     │   DEL   │     │ LFT │ DN  │ RGT │     │         │ F7  │ F8  │ F9  │     │     │     │     │
+     * │     │         │     │ LFT │ DN  │ RGT │     │         │ F7  │ F8  │ F9  │     │     │     │     │
      * ├─────┼─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-     * │     │         │     │     │     │     │     │         │ F10 │ F11 │ F12 │     │     │     │     │
+     * │     │   DEL   │     │     │     │     │     │         │ F10 │ F11 │ F12 │     │     │     │     │
      * └─────┴─────────┴─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┴─────┴─────┘
      *                       │     │ L1  │     │     │         │     │     │     │     │
      *                       └─────┴─────┴─────┴─────┘         └─────┴─────┴─────┴─────┘
@@ -73,8 +73,8 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
                           KC_NO, KC_NO,   KC_NO,   KC_PGUP, KC_HOME,      KC_F1,   KC_F2,   KC_F3,   KC_NO,  KC_NO,
               A(KC_TAB),  KC_NO, KC_NO,   KC_UP,   KC_PGDN, KC_END,       KC_F4,   KC_F5,   KC_F6,   KC_NO,  KC_NO,   KC_NO,
-      KC_NO,  KC_DEL,     KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,        KC_F7,   KC_F8,   KC_F9,   KC_NO,  KC_NO,   KC_NO,   KC_NO,
-      KC_NO,  KC_NO,      KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_F10,  KC_F11,  KC_F12,  KC_NO,  KC_NO,   KC_NO,   KC_NO,
+      KC_NO,  KC_NO,      KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,        KC_F7,   KC_F8,   KC_F9,   KC_NO,  KC_NO,   KC_NO,   KC_NO,
+      KC_NO,  KC_DEL,     KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_F10,  KC_F11,  KC_F12,  KC_NO,  KC_NO,   KC_NO,   KC_NO,
                                  KC_NO,   KC_TRNS, KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
 
