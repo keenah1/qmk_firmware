ab385d1b096a39018c01d8b9d6db173ff91c3757
keenah1 <jordan.a.rodier@gmail.com>
2024-05-24 10:17:04 -0500

    Bspc tap w/ shift hold

--- keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
+++ keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
      *                    └─────┴─────┴─────┴─────────┘         └─────┴──────┴─────┴─────┘
      */
     LAYOUT(
-                        KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,   KC_8,           KC_9,   KC_0,
-               KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,           KC_O,   KC_P,     KC_ENT,
-      KC_ESC,  KC_LSFT, KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,   KC_K,           KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
-      KC_LWIN, KC_BSPC, KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,        KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
-                                 MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_SPC, KC_ENT, MO(LAYER_SPCL), KC_NO
+                               KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,   KC_8,           KC_9,   KC_0,
+               KC_TAB,         KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,           KC_O,   KC_P,     KC_ENT,
+      KC_ESC,  SFT_T(KC_BSPC), KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,   KC_K,           KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
+      KC_LWIN, KC_NO,          KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,        KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
+                                        MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_SPC, KC_ENT, MO(LAYER_SPCL), KC_NO
     ),
 
   [LAYER_1] =
