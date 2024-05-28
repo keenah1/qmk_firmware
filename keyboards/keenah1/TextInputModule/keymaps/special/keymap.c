8559870e6e144064ca50081674491ff7256cb1cc
keenah1 <jordan.a.rodier@gmail.com>
2024-05-28 07:57:51 -0500

    Remove space from RH, double bspc pinky

--- keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
+++ keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
@@ -38,22 +38,22 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      *              ┌─────┬─────┬─────┬─────┬─────────┐         ┌─────┬──────┬─────┬─────┬─────┐
      *              │ 1/! │ 2/@ │ 3/# │ 4/$ │   5/%   │         │ 6/^ │ 7/&  │ 8\* │ 9/( │ 0/) │
-     *       ┌──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼─────┐
-     *       │ TAB  │  Q  │  W  │  E  │  R  │    T    │         │  Y  │  U   │  I  │  O  │  P  │ ENT │
-     * ┌─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼──────┐
-     * │ ESC │ SFT  │  A  │  S  │  D  │  F  │    G    │         │  H  │  J   │  K  │  L  │ ;/: │ '/" │ BSPC │
-     * ├─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼──────┤
-     * │ WIN │ BSPC │  Z  │  X  │  C  │  V  │    B    │         │  N  │  M   │ ,/< │ ./> │ //? │ \/| │ DEL  │
-     * └─────┴──────┴─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┴─────┴──────┘
-     *                    │ VSC │ L1  │ SPC │ ENT/CTL │         │ SPC │ SPCL │     │     │
+     *       ┌──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┐
+     *       │ TAB  │  Q  │  W  │  E  │  R  │    T    │         │  Y  │  U   │  I  │  O  │  P  │ BSPC │
+     * ┌─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼──────┐
+     * │ ESC │ SFT  │  A  │  S  │  D  │  F  │    G    │         │  H  │  J   │  K  │  L  │ ;/: │ '/"  │ BSPC │
+     * ├─────┼──────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼──────┤
+     * │ WIN │ BSPC │  Z  │  X  │  C  │  V  │    B    │         │  N  │  M   │ ,/< │ ./> │ //? │ \/|  │ DEL  │
+     * └─────┴──────┴─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┴──────┴──────┘
+     *                    │ VSC │ L1  │ SPC │ ENT/CTL │         │ ENT │ SPCL │     │     │
      *                    └─────┴─────┴─────┴─────────┘         └─────┴──────┴─────┴─────┘
      */
     LAYOUT(
-                               KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,   KC_8,           KC_9,   KC_0,
-               KC_TAB,         KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,           KC_O,   KC_P,     KC_ENT,
-      KC_ESC,  SFT_T(KC_BSPC), KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,   KC_K,           KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
-      KC_LWIN, KC_NO,          KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,        KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
-                                        MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_SPC, KC_ENT, MO(LAYER_SPCL), KC_NO
+                               KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,           KC_8,    KC_9,   KC_0,
+               KC_TAB,         KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,           KC_I,    KC_O,   KC_P,     KC_BSPC,
+      KC_ESC,  SFT_T(KC_BSPC), KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,           KC_K,    KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
+      KC_LWIN, KC_NO,          KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,           KC_COMM, KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
+                                        MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_ENT, MO(LAYER_SPCL), KC_NO,   KC_NO
     ),
 
   [LAYER_1] =
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
               KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
       KC_NO,  KC_TILD, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_PIPE, KC_UNDS, KC_LCBR, KC_RCBR, KC_PLUS, KC_NO,
       KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_EQL,  KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_TRNS, KC_NO
+                                KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_TRNS, KC_NO,   KC_NO
     ),
 
   [LAYER_VSC] =
