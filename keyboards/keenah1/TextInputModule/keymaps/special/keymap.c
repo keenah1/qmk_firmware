dace93cfe27cf22769be925247f023b00459ef84
keenah1 <jordan.a.rodier@gmail.com>
2024-09-29 16:40:16 -0500

    Nav layer on hold A; Alt and spcl on left thumb; rshft on right pinky

--- keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
+++ keyboards/keenah1/TextInputModule/keymaps/special/keymap.c
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layouts {
   LAYER_0,
-  LAYER_1,
+  LAYER_NAV_F,
   LAYER_SPCL,
   LAYER_VSC,
   NUM_LAYERS
@@ -36,68 +36,68 @@ enum layouts {
 const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_0] =
     /*
-     *              ┌─────┬─────┬─────┬─────┬─────────┐         ┌─────┬──────┬─────┬─────┬─────┐
-     *              │ 1/! │ 2/@ │ 3/# │ 4/$ │   5/%   │         │ 6/^ │ 7/&  │ 8\* │ 9/( │ 0/) │
-     *        ┌─────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┐
-     *        │ TAB │  Q  │  W  │  E  │  R  │    T    │         │  Y  │  U   │  I  │  O  │  P  │ BSPC │
-     * ┌──────┼─────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼──────┐
-     * │ BSPC │ SFT │  A  │  S  │  D  │  F  │    G    │         │  H  │  J   │  K  │  L  │ ;/: │ '/"  │ BSPC │
-     * ├──────┼─────┼─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼──────┤
-     * │ ESC  │ WIN │  Z  │  X  │  C  │  V  │    B    │         │  N  │  M   │ ,/< │ ./> │ //? │ \/|  │ DEL  │
-     * └──────┴─────┴─────┼─────┼─────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┴──────┴──────┘
-     *                    │ VSC │ L1  │ SPC │ ENT/CTL │         │ ENT │ SPCL │     │     │
-     *                    └─────┴─────┴─────┴─────────┘         └─────┴──────┴─────┴─────┘
+     *              ┌───────┬─────┬──────┬─────┬─────────┐         ┌─────┬──────┬─────┬─────┬─────┐
+     *              │  1/!  │ 2/@ │ 3/#  │ 4/$ │   5/%   │         │ 6/^ │ 7/&  │ 8\* │ 9/( │ 0/) │
+     *        ┌─────┼───────┼─────┼──────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┐
+     *        │ TAB │   Q   │  W  │  E   │  R  │    T    │         │  Y  │  U   │  I  │  O  │  P  │ BSPC │
+     * ┌──────┼─────┼───────┼─────┼──────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼─────┐
+     * │ BSPC │ SFT │ A/NVF │  S  │  D   │  F  │    G    │         │  H  │  J   │  K  │  L  │ ;/: │ '/"  │ SFT │
+     * ├──────┼─────┼───────┼─────┼──────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┼──────┼─────┤
+     * │ ESC  │ WIN │ Z/VSC │  X  │  C   │  V  │    B    │         │  N  │  M   │ ,/< │ ./> │ //? │ \/|  │ DEL │
+     * └──────┴─────┴───────┼─────┼──────┼─────┼─────────┤         ├─────┼──────┼─────┼─────┼─────┴──────┴─────┘
+     *                      │ ALT │ SPCL │ SPC │ ENT/CTL │         │ ENT │ SPCL │     │     │
+     *                      └─────┴──────┴─────┴─────────┘         └─────┴──────┴─────┴─────┘
      */
     LAYOUT(
-                        KC_1,    KC_2,          KC_3,        KC_4,   KC_5,               KC_6,   KC_7,           KC_8,    KC_9,   KC_0,
-               KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,   KC_T,               KC_Y,   KC_U,           KC_I,    KC_O,   KC_P,     KC_BSPC,
-      KC_BSPC, KC_LSFT, KC_A,    KC_S,          KC_D,        KC_F,   KC_G,               KC_H,   KC_J,           KC_K,    KC_L,   KC_SCLN,  KC_QUOT,  KC_BSPC,
-      KC_ESC,  KC_LWIN, KC_Z,    KC_X,          KC_C,        KC_V,   KC_B,               KC_N,   KC_M,           KC_COMM, KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
-                                 MO(LAYER_VSC), MO(LAYER_1), KC_SPC, CTL_T(KC_ENT),      KC_ENT, MO(LAYER_SPCL), KC_NO,   KC_NO
+                        KC_1,                 KC_2,    KC_3,           KC_4,   KC_5,               KC_6,   KC_7,           KC_8,    KC_9,   KC_0,
+               KC_TAB,  KC_Q,                 KC_W,    KC_E,           KC_R,   KC_T,               KC_Y,   KC_U,           KC_I,    KC_O,   KC_P,     KC_BSPC,
+      KC_BSPC, KC_LSFT, LT(LAYER_NAV_F,KC_A), KC_S,    KC_D,           KC_F,   KC_G,               KC_H,   KC_J,           KC_K,    KC_L,   KC_SCLN,  KC_QUOT,  KC_RSFT,
+      KC_ESC,  KC_LWIN, LT(LAYER_VSC,KC_Z),   KC_X,    KC_C,           KC_V,   KC_B,               KC_N,   KC_M,           KC_COMM, KC_DOT, KC_SLSH,  KC_BSLS,  KC_DEL,
+                                              KC_LALT, MO(LAYER_SPCL), KC_SPC, CTL_T(KC_ENT),      KC_ENT, MO(LAYER_SPCL), KC_NO,   KC_NO
     ),
 
-  [LAYER_1] =
+  [LAYER_NAV_F] =
     /*
-     *                 ┌─────┬─────┬─────┬─────┬─────┐         ┌─────┬─────┬─────┬─────┬─────┐
-     *                 │     │     │     │ PUP │ HME │         │ F1  │ F2  │ F3  │     │     │
-     *       ┌─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┐
-     *       │ ALT+TAB │     │     │ UP  │ PDN │ END │         │ F4  │ F5  │ F6  │     │     │     │
-     * ┌─────┼─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┐
-     * │     │   DEL   │     │ LFT │ DN  │ RGT │     │         │ F7  │ F8  │ F9  │     │     │     │     │
-     * ├─────┼─────────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-     * │     │   DEL   │     │     │     │     │     │         │ F10 │ F11 │ F12 │     │     │     │     │
-     * └─────┴─────────┴─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┴─────┴─────┘
-     *                       │     │ L1  │     │     │         │     │     │     │     │
-     *                       └─────┴─────┴─────┴─────┘         └─────┴─────┴─────┴─────┘
+     *             ┌─────┬─────┬─────┬─────┬─────┐         ┌─────┬─────┬─────┬─────┬─────┐
+     *             │     │     │     │ PUP │ HME │         │ F1  │ F2  │ F3  │     │     │
+     *       ┌─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┐
+     *       │     │     │     │ UP  │ PDN │ END │         │ F4  │ F5  │ F6  │     │     │     │
+     * ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┐
+     * │     │     │ LNF │ LFT │ DN  │ RGT │     │         │ F7  │ F8  │ F9  │     │     │     │     │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │     │     │     │     │     │     │         │ F10 │ F11 │ F12 │     │     │     │     │
+     * └─────┴─────┴─────┼─────┼─────┼─────┼─────┤         ├─────┼─────┼─────┼─────┼─────┴─────┴─────┘
+     *                   │     │     │     │     │         │     │     │     │     │
+     *                   └─────┴─────┴─────┴─────┘         └─────┴─────┴─────┴─────┘
      */
     LAYOUT(
-                          KC_NO, KC_NO,   KC_NO,   KC_PGUP, KC_HOME,      KC_F1,   KC_F2,   KC_F3,   KC_NO,  KC_NO,
-              A(KC_TAB),  KC_NO, KC_NO,   KC_UP,   KC_PGDN, KC_END,       KC_F4,   KC_F5,   KC_F6,   KC_NO,  KC_NO,   KC_NO,
-      KC_NO,  KC_DEL,     KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,        KC_F7,   KC_F8,   KC_F9,   KC_NO,  KC_NO,   KC_NO,   KC_NO,
-      KC_NO,  KC_DEL,     KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_F10,  KC_F11,  KC_F12,  KC_NO,  KC_NO,   KC_NO,   KC_NO,
-                                 KC_NO,   KC_TRNS, KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO
+                      KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_HOME,      KC_F1,   KC_F2,   KC_F3,   KC_NO,  KC_NO,
+              KC_NO,  KC_NO,   KC_NO,   KC_UP,   KC_PGDN, KC_END,       KC_F4,   KC_F5,   KC_F6,   KC_NO,  KC_NO,   KC_NO,
+      KC_NO,  KC_NO,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,        KC_F7,   KC_F8,   KC_F9,   KC_NO,  KC_NO,   KC_NO,   KC_NO,
+      KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_F10,  KC_F11,  KC_F12,  KC_NO,  KC_NO,   KC_NO,   KC_NO,
+                               KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
 
   [LAYER_SPCL] =
     /*
-     *             ┌─────┬─────┬─────┬─────┬─────┐         ┌─────┬──────┬─────┬─────┬─────┐
-     *             │     │     │     │     │     │         │     │      │     │     │     │
-     *       ┌─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┐
-     *       │  `  │  !  │  @  │  #  │  $  │  %  │         │  ^  │  &   │  *  │  (  │  )  │  -  │
-     * ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼─────┐
-     * │     │  ~  │     │  {  │  (  │     │     │         │  |  │  _   │  )  │  }  │     │  +  │     │
-     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼─────┤
-     * │     │     │     │  [  │  <  │  +  │  =  │         │  =  │  -   │  >  │  ]  │     │  =  │     │
-     * └─────┴─────┴─────┼─────┼─────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┴─────┴─────┘
-     *                   │     │     │     │     │         │     │ SPCL │     │     │
-     *                   └─────┴─────┴─────┴─────┘         └─────┴──────┴─────┴─────┘
+     *             ┌─────┬─────┬──────┬─────┬─────┐         ┌─────┬──────┬─────┬─────┬─────┐
+     *             │     │     │      │     │     │         │     │      │     │     │     │
+     *       ┌─────┼─────┼─────┼──────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┐
+     *       │  `  │  !  │  @  │  #   │  $  │  %  │         │  ^  │  &   │  *  │  (  │  )  │     │
+     * ┌─────┼─────┼─────┼─────┼──────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼─────┐
+     * │ DEL │  ~  │     │  {  │  (   │     │     │         │  |  │  _   │  )  │  }  │     │     │     │
+     * ├─────┼─────┼─────┼─────┼──────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │     │     │  [  │  <   │  +  │  =  │         │  =  │  -   │  >  │  ]  │     │     │     │
+     * └─────┴─────┴─────┼─────┼──────┼─────┼─────┤         ├─────┼──────┼─────┼─────┼─────┴─────┴─────┘
+     *                   │     │ SPCL │     │     │         │     │ SPCL │     │     │
+     *                   └─────┴──────┴─────┴─────┘         └─────┴──────┴─────┴─────┘
      */
     LAYOUT(
                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-              KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
-      KC_NO,  KC_TILD, KC_NO,   KC_LCBR, KC_LPRN, KC_NO,   KC_NO,        KC_PIPE, KC_UNDS, KC_RPRN, KC_RCBR, KC_NO,   KC_PLUS, KC_NO,
-      KC_NO,  KC_NO,   KC_NO,   KC_LBRC, KC_LABK, KC_PLUS, KC_EQL,       KC_EQL,  KC_MINS, KC_RABK, KC_RBRC, KC_NO,   KC_EQL,  KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_TRNS, KC_NO,   KC_NO
+              KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
+      KC_DEL, KC_TILD, KC_NO,   KC_LCBR, KC_LPRN, KC_NO,   KC_NO,        KC_PIPE, KC_UNDS, KC_RPRN, KC_RCBR, KC_NO,   KC_NO, KC_NO,
+      KC_NO,  KC_NO,   KC_NO,   KC_LBRC, KC_LABK, KC_PLUS, KC_EQL,       KC_EQL,  KC_MINS, KC_RABK, KC_RBRC, KC_NO,   KC_NO, KC_NO,
+                                KC_NO,   KC_TRNS, KC_NO,   KC_NO,        KC_NO,   KC_TRNS, KC_NO,   KC_NO
     ),
 
   [LAYER_VSC] =
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_NO,         KC_NO,   KC_NO,   KC_NO,            KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                KC_NO,   VSC_CMNT_LINE, KC_NO,   KC_NO,   VSC_INS_LINE_ABV, VSC_UNFLD_REGION,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO,   KC_NO,   VSC_CMNT_BLK,  KC_NO,   KC_NO,   VSC_INS_LINE_BLW, VSC_FLD_REGION,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,            KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                       KC_TRNS, KC_NO,   KC_NO,            KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO
+      KC_NO,   KC_NO,   KC_TRNS,       KC_NO,   KC_NO,   KC_NO,            KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                       KC_NO,   KC_NO,   KC_NO,            KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
